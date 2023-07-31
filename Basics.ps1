af9079b4c591a43f884553e814ab7ab26478b09c
DaHussl <hussam.alkobati@yahoo.de>
2023-07-31 22:30:44 +0200

    learn basics

---
+++ Basics.ps1
@@ -0,0 +1,15 @@
+#verify the setting with 
+Get-ExecutionPolicy
+
+#change the execution policy
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned 
+
+#get the computername 
+hostname 
+
+#retrieves data from a resource
+Get-Childitem
+
+#selects a resource in a container
+Select-Object
+
