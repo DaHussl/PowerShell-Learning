1246d9344b5f21e3e2d9a87363a225ee82d79615
DaHussl <hussam.alkobati@yahoo.de>
2023-08-03 23:10:18 +0200

    learn basics

--- Basics.ps1
+++ Basics.ps1
@@ -13,3 +13,27 @@ Get-Childitem
 #selects a resource in a container
 Select-Object
 
+#equal or not equal 
+-eq   |   -ne 
+
+#greater or less than 
+-gt   |   -lt  
+
+#great than or equal to 
+-ge    
+
+#less than or equal to
+-le  
+
+#match a string using * wildcard or not
+-Like   |   -NotLike  
+
+#matches or not the specified regular expression 
+-Match   |   -NotMatch 
+
+#collection contains a specified value or not
+-contains   |   -NotContains  
+
+#replace specified value 
+-Replace  
+
